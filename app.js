099520ce39450d4f52f0148d1e57187f26ad9f72
Ilia Kulikov <kulikov.ilya@gmail.com>
2022-08-01 12:20:16 +0100

    deployment issue

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// Parse the URL parameter
+// Parse the URL parameters
   function getParameterByName(name, url) {
       if (!url) url = window.location.href;
       name = name.replace(/[\[\]]/g, "\\$&");
